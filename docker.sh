bb535a2160672f20876af384499d58ad0332ca2e
maheshdongare1983 <maheshdongare1983@gmail.com>
2019-04-11 15:07:03 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -10,5 +10,4 @@ COPY addressbook.war /usr/local/tomcat/webapps/myapp.war
 CMD "catalina.sh" "run"
 EXPOSE 8080 
 EOT
-docker build -t kubedemon/deploy_image:$BUILD_NUMBER .
-docker run -itd --name=mycontainer-$BUILD_NUMBER -p 8080:8080 kubedemon/deploy_image:$BUILD_NUMBER
+
